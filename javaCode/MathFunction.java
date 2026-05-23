27d6ad3cbe77e5a3512e84f15f3623e28b699c43
birbalkr <birbalkr1435@gmail.com>
2026-05-23 09:44:47 +0530

    MathFunction

---
+++ javaCode/MathFunction.java
@@ -0,0 +1,18 @@
+package javaCode;
+
+public class MathFunction {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-5));      // Absolute value
+        System.out.println(Math.max(10, 20));  // Maximum of two numbers
+        System.out.println(Math.min(10, 20));  // Minimum of two numbers
+        System.out.println(Math.sqrt(16));     // Square root
+
+        System.out.println(Math.floor(3.7));   // Floor value
+        System.out.println(Math.ceil(3.8));    // Ceiling value
+        System.out.println(Math.round(3.5));   // Round value
+        System.out.println(Math.pow(2, 3));    // Power function
+        System.out.println(Math.cbrt(27));     // Cube root
+    }
+}
