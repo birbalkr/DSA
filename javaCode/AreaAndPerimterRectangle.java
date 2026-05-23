bbfecdcac28f37dbcc54219b4c9c7cb0e58fa881
birbalkr <birbalkr1435@gmail.com>
2026-05-23 09:53:03 +0530

    Area And Perimter of a Rectangle

---
+++ javaCode/AreaAndPerimterRectangle.java
@@ -0,0 +1,14 @@
+package javaCode;
+
+import java.util.Scanner;
+
+public class AreaAndPerimterRectangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int length=sc.nextInt();
+        int width=sc.nextInt();
+
+        System.out.println(length*width);
+        System.out.println(2*(length+width));
+    }
+}
