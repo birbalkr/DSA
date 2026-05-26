a6c88ceb917063dffeb82d9bcbfa8693ab38e78d
Birbal Kumar <153169574+birbalkr@users.noreply.github.com>
2026-05-26 08:08:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.js
@@ -0,0 +1,18 @@
+class Solution {
+    fizzBuzz(number) {
+        // code here
+        if (number % 3 == 0 && number % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+    else if (number % 3 == 0) {
+        console.log( "Fizz");
+    }
+    else if (number % 5 == 0) {
+        console.log( "Buzz");
+    }
+    else {
+        console.log( number);
+    }
+        
+    }
+}
