be3900f0c036d1857d4caf68cb33c4584cf1629a
Birbal Kumar <153169574+birbalkr@users.noreply.github.com>
2026-05-27 08:24:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.js
@@ -0,0 +1,14 @@
+class Solution {
+    solve(n) {
+        // Code here
+        if(n<=100){
+            console.log("Number");
+        }
+        else{
+            console.log("Big");
+            console.log("Number");
+        }
+          
+        
+    }
+}
