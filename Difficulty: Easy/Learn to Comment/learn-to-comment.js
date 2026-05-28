f4645c9145d0bee5e05f6476fa67cbeb2eba32dc
Birbal Kumar <153169574+birbalkr@users.noreply.github.com>
2026-05-28 07:44:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+let a = input[0];
+let b = input[1];
+let c = input[2];
+
+// Prints a
+console.log(a);
+
+// Prints b
+// console.log(b);
+
+// Prints c
+console.log(c);
